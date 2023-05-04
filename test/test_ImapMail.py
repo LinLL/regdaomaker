5a2cad1ddd13cb5c0c46913aae0045d25f2bd7ef
LGrok <luolin.work@gmail.com>
2023-05-04 15:38:13 +0800

    测试状态

---
+++ test/test_ImapMail.py
@@ -0,0 +1,17 @@
+from mailutil.ImapMail import ImapMail
+def getOneTestMailPwd():
+    with open("data/mailspwd", 'r') as fr:
+        for l in fr.readlines():
+            mail, pwd = l.split("|")
+            pwd = pwd.strip()
+            mail = mail.strip()
+            return mail, pwd
+def test_checkStatus():
+    mail, pwd = getOneTestMailPwd()
+
+    testImapMail = ImapMail(mail, pwd)
+    print(testImapMail.mail)
+    print(testImapMail.passwd)
+
+    result = testImapMail.checkStatus()
+    assert result == True
