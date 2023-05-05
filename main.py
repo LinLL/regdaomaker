bae1687638bb20edfbb6bb8ab7fd8a7f1cb776eb
LGrok <luolin.work@gmail.com>
2023-05-05 14:58:30 +0800

    入口函数

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import time
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from RegDaoMaker import RegDaoMaker
+from mailutil.ImapMail import ImapMail
+
+def run():
+    testRegDaoMaker = RegDaoMaker()
+    testRegDaoMaker.reg("jeeanmoodiex@hotmail.com","q1w2e3R$")
+
+    myMail = ImapMail("jeeanmoodiex@hotmail.com","AHFAGA32")
+    msg = myMail.searchSender("daomaker")
+    verify_url = myMail.parseRegMail(msg)
+    testRegDaoMaker.driver.get(verify_url)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    run()
